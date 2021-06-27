e495690577e340a42bf9edf1f4a5bb6563fdaeb5
Robert Garner <Robert@sohoits-MacBook-Air.local>
2021-06-27 17:01:44 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let sky = document.getElementById('sky');
+let moon = document.getElementById('moon');
+let mountain = document.getElementById('mountain');
+let road = document.getElementById('road');
+let text = document.getElementById('text');
+
+window.addEventListener('scroll', function() {
+  var value = window.scrollY;
+
+  sky.style.top = value * 0.5 + 'px';
+  moon.style.left = -value * 0.5 + 'px';
+  mountain.style.top = -value * 0.15 + 'px';
+  road.style.top = value * 0.15 + 'px';
+  text.style.top = value * 1 + 'px';
+})
